53d13f20b5c5465642b8fb7da434340ecd1578f4
Gabriel Santos <gabrielfndsts265@gmail.com>
2024-10-01 03:13:01 -0300

    terceiro commit, refatoração

--- TestFranc.java
+++ TestFranc.java
@@ -4,9 +4,18 @@ import org.junit.Test;
 public class TestFranc {
 
     @Test
+
+public void testEquality() {
+   assertTrue(Money.dollar(5).equals(Money.dollar(5)));
+   assertFalse(Money.dollar(5).equals(Money.dollar(6)));
+   assertTrue(Money.franc(5).equals(Money.franc(5)));
+   assertFalse(Money.franc(5).equals(Money.franc(6)));
+   assertFalse(Money.franc(5).equals(Money.dollar(5)));
+}
+
     public void testFrancMultiplication() {
-   Franc five = new Franc(5);
-   assertEquals(new Franc(10), five.times(2));
-   assertEquals(new Franc(15), five.times(3));
+   Money five = Money.franc(5);
+   assertEquals(Money.franc(10), five.times(2));
+   assertEquals(Money.franc(15), five.times(3));
 }
 }
